a8a438a3cfdbe2ec2b02c39ff1e4acde5166e132
sagar.jirapure <sagar.jirapure@winwire.com>
2021-03-17 13:58:12 +0530

    removed unnessery code

--- Encoder/Encoder.cs
+++ Encoder/Encoder.cs
@@ -32,7 +32,6 @@ namespace Encoder
             m.Add('o', 4);
             m.Add('u', 5);
 
-            char[] arr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             _message = _Output.ToString();
             string OutPut = "";
             char[] charArray = { };
